21c22dc1972222e3770553f276396faf13ce1a42
Jafer Daltro <jaferjr@gmail.com>
2020-12-10 05:40:09 -0300

    created seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_201718) do
+ActiveRecord::Schema.define(version: 2020_12_09_223108) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "country"
     t.string "city"
     t.string "state"
     t.string "neighborhood"
-    t.string "street"
+    t.string "public_place"
     t.string "number"
     t.integer "client_id", null: false
-    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["client_id"], name: "index_addresses_on_client_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#USUARIOS
+User.create name: 'José', status: :active, kind: :salesman
+User.create name: 'Marcos', status: :active, kind: :manager
+
+Address.create(country:'Brasil', city:'Fortaleza', state:'Ceará', neighborhood:'Salinas', public_place:'Av.Washington Soares', number:'123')
+Address.create(country:'Brasil', city:'Fortaleza', state:'Ceará', neighborhood:'Salinas', public_place:'Shopping Salinas', number:'123')
+Address.create(country:'Brasil', city:'Fortaleza', state:'Ceará', neighborhood:'Salinas', public_place:'Av.Santos Dummont, 343', number:'123')
+Address.create(country:'Brasil', city:'São Paulo', state:'São Paulo', neighborhood:'Salinas', public_place:'Alameda Gabriel Monteiro da Silva', number:'2113')
+
+#FORNECEDOR
+Vendor.create(description:'Casatto',contact_person:'Andrea', phone:'85-3278-1086', address_id: 2, cnpj:'79437997979')
+Vendor.create(description:'Okna Persianas',contact_person:'Okna', phone:'85-23234444', address_id: 4, cnpj:'79437997979')
+Vendor.create(description:'Donatelli',contact_person:'Dona', phone:'85-23234444', address_id: 3, cnpj:'79437997979')
+Vendor.create(description:'Persianas New York',contact_person:'York', phone:'85-23234444', address_id: 1, cnpj:'79437997979')
+
+#CATEGORIAS
+Category.create(description:'Papel de Parede')
+Category.create(description:'Cortina')
+Category.create(description:'Cabeceira')
+Category.create(description:'Revestimento')
+
+#PRODUTO - PAPEL DE PAREDE
+Product.create(description:'Clássicos', vendor_id:1, category_id: 1)
+Product.create(description:'Temáticos', vendor_id:1, category_id: 1)
+Product.create(description:'Contemporâneo', vendor_id:1, category_id: 1)
+Product.create(description:'Listrados', vendor_id:1, category_id: 1)
+Product.create(description:'Elementos Naturais', vendor_id:1, category_id: 1)
+Product.create(description:'Florais', vendor_id:1, category_id: 1)
+Product.create(description:'Lisos', vendor_id:1, category_id: 1)
+Product.create(description:'Texturas', vendor_id:1 ,category_id: 1 )
+
+#PRODUTO - CORTINA
+Product.create(description:'Tecidos', vendor_id:3 ,category_id: 2 )
+Product.create(description:'Lumina', vendor_id:2 ,category_id: 2 )
+Product.create(description:'Celular', vendor_id:2 ,category_id: 2 )
+Product.create(description:'Plissada', vendor_id:2 ,category_id: 2 )
+
+#PRODUTO - REVESTIMENTO
+Product.create(description:'Plissada', vendor_id:3 ,category_id: 4 )
+Product.create(description:'Plissada', vendor_id:3 ,category_id: 4 )
+Product.create(description:'Plissada', vendor_id:3 ,category_id: 4 )
+Product.create(description:'Plissada', vendor_id:3 ,category_id: 4 )
+
+# CLIENTES
+Client.create name: 'Paulo', cpf_cnpj: '1234', email: 'paulo@google.com'
+Client.create name: 'Julia', cpf_cnpj: 'abcd', email: 'julia@google.com'
